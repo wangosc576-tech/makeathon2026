0d62a582a941f5b8b39467f0fe3e6c1c19f8b110
Princeden Hom <princedenhom@gmail.com>
2026-02-21 20:30:57 -0500

    change config + reqs

--- config.py
+++ config.py
@@ -8,8 +8,6 @@ FLASHLIGHT_PIN = 18
 IR_SENSOR_PIN = 24
 PWM_FREQ = 1000
 
-# Set True when running without a monitor attached
-HEADLESS = False
 
 # How long (seconds) gesture detection stays awake after
 # the IR sensor last detected something
@@ -23,3 +21,8 @@ DEBOUNCE_FRAMES = 8
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
 JPG_QUALITY = 50
+
+# LED pins
+LED_RED = 37
+LED_GREEN = 38
+LED_BLUE = 40
